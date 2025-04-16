f780760e521f78b471cbcbf07bb710b15a3526fa
sonikk <vasunina_sofia@bk.ru>
2025-04-16 16:46:20 +0300

    some changes in hash_table.hpp

--- hash_table_prj/hash_table/hash_table.hpp
+++ hash_table_prj/hash_table/hash_table.hpp
@@ -16,6 +16,8 @@ private:
     std::hash<KeyType> hash_function;
 
 public:
+    HashTable(size_t size = 0);  
+    ~HashTable();
 
     void insert(const KeyType& key, const ValueType& value);
     bool contains(const KeyType& key) const;
@@ -38,4 +40,94 @@ public:
     Iterator end() const;
 
     };
+};
+
+
+
+template<typename KeyType, typename ValueType>
+HashTable<KeyType, ValueType>::HashTable(size_t size) : table(size, nullptr) {}
+
+
+template<typename KeyType, typename ValueType>
+HashTable<KeyType, ValueType>::~HashTable() {
+    for (auto& bucket : table) {
+        while (bucket) {
+            Node* temp = bucket;
+            bucket = bucket->next;
+            delete temp;
+        }
+    }
+}
+
+template<typename KeyType, typename ValueType>
+void HashTable<KeyType, ValueType>::insert(const KeyType& key, const ValueType& value) {
+    if (table.empty()) {
+        throw std::out_of_range("HashTable is empty");
+    }
+    
+    size_t index = hash_function(key) % table.size();
+    Node* current = table[index];
+    
+    while (current) {
+        if (current->key == key) {
+            current->value = value;
+            return;
+        }
+        current = current->next;
+    }
+    
+    Node* newNode = new Node(key, value);
+    newNode->next = table[index];
+    table[index] = newNode;
+}
+
+template<typename KeyType, typename ValueType>
+bool HashTable<KeyType, ValueType>::contains(const KeyType& key) const {
+    size_t index = hash_function(key) % table.size();
+    Node* current = table[index];
+    
+    while (current) {
+        if (current->key == key) {
+            return true;
+        }
+        current = current->next;
+    }
+    return false;
+}
+
+template<typename KeyType, typename ValueType>
+ValueType& HashTable<KeyType, ValueType>::operator[](const KeyType& key) {
+    if (table.empty()) {
+        throw std::out_of_range("HashTable is empty");
+    }
+    
+    size_t index = hash_function(key) % table.size();
+    Node* current = table[index];
+    
+    while (current) {
+        if (current->key == key) {
+            return current->value;
+        }
+        current = current->next;
+    }
+    
+    Node* newNode = new Node(key, ValueType());
+    newNode->next = table[index];
+    table[index] = newNode;
+    return newNode->value;
+}
+
+template<typename KeyType, typename ValueType>
+typename HashTable<KeyType, ValueType>::Iterator HashTable<KeyType, ValueType>::begin() {
+    for (size_t i = 0; i < table.size(); ++i) {
+        if (table[i]) {
+            return Iterator(i, table[i], table);
+        }
+    }
+    return end();
+}
+
+template<typename KeyType, typename ValueType>
+typename HashTable<KeyType, ValueType>::Iterator::end() {
+    return Iterator(table.size(), nullptr, table)
 };
